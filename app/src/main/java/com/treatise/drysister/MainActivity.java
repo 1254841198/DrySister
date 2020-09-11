0537c39c39ec68e784354b9d5e0e5698bdaa6350
yangqifeng <1254841198@qq.com>
2020-09-11 17:01:23 +0800

    frist commit

---
+++ app/src/main/java/com/treatise/drysister/MainActivity.java
@@ -0,0 +1,16 @@
+package com.treatise.drysister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
