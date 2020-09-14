bcec430103f7df42ec2fef55fe4d596fe8c2daf9
yangqifeng <1254841198@qq.com>
2020-09-14 13:27:10 +0800

    图片初始加载

--- app/src/main/java/com/treatise/drysister/MainActivity.java
+++ app/src/main/java/com/treatise/drysister/MainActivity.java
@@ -3,14 +3,60 @@ package com.treatise.drysister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+
+    ArrayList<String> urls;
+
+    PictureLoader loader;
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initData();
+        initView();
 
+    }
+
+    private void initData() {
+        loader = new PictureLoader();
+        urls = new ArrayList<>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+    private void initView() {
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.changeImage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.changeImage) {
+                    loader.load(imageView, urls.get(i++));
+                    i = i > 9 ? 0 : i;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/treatise/drysister/PictureLoader.java
@@ -0,0 +1,75 @@
+package com.treatise.drysister;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PictureLoader {
+
+    public static final int SUCCESS = 1;
+
+    String imageUrl;
+    ImageView imageView;
+    byte[] picBytes;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == SUCCESS) {
+                if (picBytes != null) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(picBytes, 0, picBytes.length);
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    };
+
+
+    public void load(ImageView imageView, String url) {
+        this.imageUrl = url;
+        this.imageView = imageView;
+        Drawable drawable = imageView.getDrawable();
+
+        new Thread(runnable).start();
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(imageUrl);
+                HttpURLConnection connection = ((HttpURLConnection) url.openConnection());
+                connection.setRequestMethod("GET");
+                connection.setReadTimeout(10000);
+                if (connection.getResponseCode() == 200) {
+                    InputStream in = connection.getInputStream();//获取输入流
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte[] bytes = new byte[1024];
+                    while (in.read(bytes) != -1) {
+                        out.write(bytes);
+                    }
+                    picBytes = out.toByteArray();
+                    in.close();
+                    out.close();
+                    handler.sendEmptyMessage(SUCCESS);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+}
